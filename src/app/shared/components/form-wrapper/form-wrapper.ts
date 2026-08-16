3e071283bfaa5e2e05aa072b961ce0a6120102ab
alepag23 <alessandro2ap3@gmail.com>
2026-08-16 23:01:36 +0200

    feat: form-wrapper ts

---
+++ src/app/shared/components/form-wrapper/form-wrapper.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-wrapper',
+  imports: [],
+  templateUrl: './form-wrapper.html',
+  styleUrl: './form-wrapper.scss',
+})
+export class FormWrapper {}
